315f45fcb1b5b021582e951a49325880dc5e3944
adriculous <admarpad@gmail.com>
2022-05-21 15:54:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cool Gitty Peeps: A GitHub Profiles Search App
+
+[EOF]
+
+## About this App
+
+All around the world, there are countless and countless of [GitHub](https://github.com) Profiles being created and updated every single day. There are also some profiles that simply exist but no longer updated or remained untouched. Sometimes, just by searching and following select profiles can get cluttered through your GitHub profile alone. What if you like to have a handy simple list of profiles you'd like to follow the next time you log into your GitHub profile without going through the hassle of searching for them all over again?
+[EOF]
+
+The Cool Gitty Peeps search app is a simple app used as personal list of GitHub profiles you are interested in following or watching them from your own Github profile. Each profile that you search for also includes the number of repos they have, their location, as well as their Twitter account for you to reach out to that particular user. The profile results should provide you the username, the number of that user's followers, the number of users following that user, the user's Twitter handle, and their location.
---
+++ src/app.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /* [MY DELIVERABLES]
+      1. As a user, I should be able to enter a GitHub username on the input/search field.
+      
+      2. As a user, I should be able to click a submit (Search) button.
+      
+      3. As a user, I expect to see the results (or no results) of the username I enter at the DOM.
+
+      4. As a user, I would be able to click on the result profile that would lead me to the actual GitHub profile.
+
+      */
+
+    const API_URL = "https://api.github.com/users/";
+
+    const userCard = document.getElementById('userCard');
+    const form = document.getElementById('form');
+    const search = document.getElementById('search');
+    const notFound = document.getElementById('notfound')
+    const notFoundText = document.getElementById('notfound-message')
+    notFound.className = 'hidden'
+
+    // getUser("adriculous");
+
+    function getUser(username) {
+        return fetch(API_URL + username, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+        })
+            .then(resp => resp.json())
+            .then(obj => {
+                // userCard(obj);
+                console.log(obj);
+            })
+            .catch((err) => {
+                notFound.removeAttribute('class')
+                notFoundText.textContent = err
+                setTimeout(() => { err.setAttribute('class', 'hidden') }, 3000)
+            })
+
+        /* function userCard(user) {
+            const profileCard = document.getElementsByClassName('userCard')
+              console.log(profileCard)/* `
+            <div class="card">
+                <div>
+                    <img class="avatar card-img-top" src="${user.avatar_url}" alt="${user.name}" />
+                </div>
+                <div class="user-info card-body">
+                    <h3 class="card-title">${user.name}</h3>
+                        <p class="card-text">${user.bio}</p>
+                        <ul class="github-info">
+                            <li><strong>Followers:</strong> ${user.followers}</li>
+                            <li><strong>Following:</strong> ${user.following}</li>
+                            <li><strong>Repos:</strong> ${user.public.repos}</li>
+                            <li><strong>Twitter:</strong> ${user.twitter}</li>
+                            <li><strong>Location:</strong> ${user.location}</li>
+                        </ul>
+                        <a href="${url} + ${username}" class="btn btn-primary">Visit Profile</a>
+                </div>
+            </div>
+        `; 
+ 
+            results.innerHTML = profileCard;
+        } */
+
+    }
+    // console.log(form)
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        // e.target.value
+        // What is "e.target.value"? (button)
+        console.log(e.target)
+        const user = search.value;
+        if (user) {
+            getUser(user);
+            search.value = "";
+        }
+        form.reset();
+    });
+
+
+})
